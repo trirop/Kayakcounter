0c4da4aa76c88149f8d90eea7ac1a97002418d64
Tristan <tristan@roppel.de>
2020-08-20 16:04:30 +0300

    training scripts added

---
+++ scripts/training/train_model_ssd_mobilenet_v1_coco.sh
@@ -0,0 +1,2 @@
+python train.py --logtostderr --train_dir=../../workspace/training_demo/training/ --pipeline_config_path=../../workspace/training_demo/training/ssd_mobilenet_v1_coco.config --model_dir=../../workspace/training_demo/pre-trained-model/ssd_mobilenet_v1_coco
+
---
+++ scripts/training/train_model_ssd_mobilenet_v2_coco.sh
@@ -0,0 +1,2 @@
+python train.py --logtostderr --train_dir=../../workspace/training_demo/training/ --pipeline_config_path=../../workspace/training_demo/training/ssd_mobilenet_v2_coco.config --model_dir=../../workspace/pre-trained_model/ssdlite_mobilenet_v2_coco/
+
