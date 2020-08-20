b13c4aa1e045af23cb15bf0de41df615367bd50d
Tristan <tristan@roppel.de>
2020-08-20 14:31:06 +0300

    Training scripts adapted

---
+++ scripts/training/train_model.sh
@@ -0,0 +1,2 @@
+python train.py --train_dir=../../../workspace/training_demo/training/ --pipeline_config_path=../../../workspace/training_demo/training/ssdlite_mobilenet_v3_small_coco.config --model_dir=../../../workspace/training_demo/pre-trained-model/ssd_mobilenet_v3_small_coco_2020_01_14/
+
