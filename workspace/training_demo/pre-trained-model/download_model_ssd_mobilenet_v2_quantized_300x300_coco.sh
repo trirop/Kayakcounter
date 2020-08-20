3ab33439e912c2373da8297b0de2d6404c9d489e
Tristan <tristan@roppel.de>
2020-08-20 16:38:44 +0300

    download_models.added

---
+++ workspace/training_demo/pre-trained-model/download_model_ssd_mobilenet_v2_quantized_300x300_coco.sh
@@ -0,0 +1,6 @@
+# Download SSD MobileNet V2 model
+# https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf1_detection_zoo.md
+curl -O http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_quantized_300x300_coco_2019_01_03.tar.gz
+tar xzf ssd_mobilenet_v2_quantized_300x300_coco_2019_01_03.tar.gz -C ../../../workspace/training_demo/pre-trained-model/
+rm ssd_mobilenet_v2_quantized_300x300_coco_2019_01_03.tar.gz
+mv ssd_mobilenet_v2_quantized_300x300_coco_2019_01_03 ssd_mobilenet_v2_quantized_300x300_coco
---
+++ workspace/training_demo/pre-trained-model/download_model_ssd_mobilenet_v3_small_coco.sh
@@ -0,0 +1,6 @@
+# Download SSD MobileNet V2 model
+# https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf1_detection_zoo.md
+curl -O http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v3_small_coco_2020_01_14.tar.gz
+tar xzf ssd_mobilenet_v3_small_coco_2020_01_14.tar.gz -C ../../../workspace/training_demo/pre-trained-model/
+rm ssd_mobilenet_v3_small_coco_2020_01_14.tar.gz
+mv ssd_mobilenet_v3_small_coco_2020_01_14 ssd_mobilenet_v3_small_coco_2020
