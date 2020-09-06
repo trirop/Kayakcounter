d5aae3327aace30a3ebb9fdc76c55760e201d27e
Tristan <tristan@roppel.de>
2020-09-06 08:37:06 +0300

    update of training scripts

---
+++ scripts/training/train_model_ssd_mobilenet_v2_quantized_300x300.sh
@@ -0,0 +1,6 @@
+python train.py \
+--logtostderr \
+--train_dir=../../workspace/training_demo/training/ \
+--pipeline_config_path=../../workspace/training_demo/training/ssd_mobilenet_v2_quantized_300x300.config \
+--model_dir=../../workspace/training_demo/pre-trained-model/ssd_mobilenet_v2_quantized_300x300 \
+
